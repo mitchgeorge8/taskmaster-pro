19cae48dee38f74959be3db1ae6b22ef3c39817c
Mitchell George <mitchgeorge8@gmail.com>
2022-03-09 20:37:01 -0500

    5.4.4 Add jquery uis datepicker

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,11 +100,20 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
+  // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -220,6 +229,10 @@ $("#trash").droppable({
   drop: function(event, ui) {
     ui.draggable.remove();
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // load tasks for the first time
